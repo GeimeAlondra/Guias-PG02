b9c721fc87ff9223c725fc461ea90ec3859a41a5
GeimeAlondra <105683683+GeimeAlondra@users.noreply.github.com>
2024-09-06 15:09:05 -0600

    Guía XIX - ADO.NET Capa desconectada (finalizada)

---
+++ CapaDesconectada/CapaDesconectada/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace CapaDesconectada
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.gridNoTipado = new System.Windows.Forms.DataGridView();
+            this.gridTipado = new System.Windows.Forms.DataGridView();
+            this.btnObtenerNoTipado = new System.Windows.Forms.Button();
+            this.btnObtenerTipado = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.gridNoTipado)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridTipado)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnObtenerNoTipado);
+            this.groupBox1.Controls.Add(this.gridNoTipado);
+            this.groupBox1.Location = new System.Drawing.Point(29, 28);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(351, 322);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "DataSet no Tipado";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btnObtenerTipado);
+            this.groupBox2.Controls.Add(this.gridTipado);
+            this.groupBox2.Location = new System.Drawing.Point(408, 28);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(362, 322);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "DataSet Tipado";
+            // 
+            // gridNoTipado
+            // 
+            this.gridNoTipado.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gridNoTipado.Location = new System.Drawing.Point(6, 29);
+            this.gridNoTipado.Name = "gridNoTipado";
+            this.gridNoTipado.Size = new System.Drawing.Size(339, 211);
+            this.gridNoTipado.TabIndex = 0;
+            // 
+            // gridTipado
+            // 
+            this.gridTipado.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gridTipado.Location = new System.Drawing.Point(6, 29);
+            this.gridTipado.Name = "gridTipado";
+            this.gridTipado.Size = new System.Drawing.Size(350, 211);
+            this.gridTipado.TabIndex = 0;
+            // 
+            // btnObtenerNoTipado
+            // 
+            this.btnObtenerNoTipado.Location = new System.Drawing.Point(100, 273);
+            this.btnObtenerNoTipado.Name = "btnObtenerNoTipado";
+            this.btnObtenerNoTipado.Size = new System.Drawing.Size(150, 23);
+            this.btnObtenerNoTipado.TabIndex = 1;
+            this.btnObtenerNoTipado.Text = "Obtener Datos No Tipados";
+            this.btnObtenerNoTipado.UseVisualStyleBackColor = true;
+            this.btnObtenerNoTipado.Click += new System.EventHandler(this.btnObtenerNoTipado_Click);
+            // 
+            // btnObtenerTipado
+            // 
+            this.btnObtenerTipado.Location = new System.Drawing.Point(111, 273);
+            this.btnObtenerTipado.Name = "btnObtenerTipado";
+            this.btnObtenerTipado.Size = new System.Drawing.Size(142, 23);
+            this.btnObtenerTipado.TabIndex = 1;
+            this.btnObtenerTipado.Text = "Obtener Datos Tipados";
+            this.btnObtenerTipado.UseVisualStyleBackColor = true;
+            this.btnObtenerTipado.Click += new System.EventHandler(this.btnObtenerTipado_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.gridNoTipado)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridTipado)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btnObtenerNoTipado;
+        private System.Windows.Forms.DataGridView gridNoTipado;
+        private System.Windows.Forms.Button btnObtenerTipado;
+        private System.Windows.Forms.DataGridView gridTipado;
+    }
+}
+
---
+++ CapaDesconectada/CapaDesconectada/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CapaDesconectada
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnObtenerNoTipado_Click(object sender, EventArgs e)
+        {
+            // Asignar un elemento iterable
+            DataTable dataTable = new DataTable();
+
+            String selectAll = "";
+            selectAll = selectAll + "SELECT [CustomerID] " + "\n";
+            selectAll = selectAll + "      ,[CompanyName] " + "\n";
+            selectAll = selectAll + "      ,[ContactName] " + "\n";
+            selectAll = selectAll + "      ,[ContactTitle] " + "\n";
+            selectAll = selectAll + "      ,[Address] " + "\n";
+            selectAll = selectAll + "      ,[City] " + "\n";
+            selectAll = selectAll + "      ,[Region] " + "\n";
+            selectAll = selectAll + "      ,[PostalCode] " + "\n";
+            selectAll = selectAll + "      ,[Country] " + "\n";
+            selectAll = selectAll + "      ,[Phone] " + "\n";
+            selectAll = selectAll + "      ,[Fax] " + "\n";
+            selectAll = selectAll + "  FROM [dbo].[Customers]";
+
+            var conexion = @"Data Source=.\SQLEXPRESS;Initial Catalog=Northwind;Integrated Security=True;";
+            
+            // Controla DataSet
+            SqlDataAdapter adapter = new SqlDataAdapter(selectAll, conexion);
+
+            // Los resultados de DataAdapet se almacenan en dataTable
+            adapter.Fill(dataTable);
+
+            // Muestra los datos en el form
+            gridNoTipado.DataSource = dataTable;
+        
+        }
+
+        private void btnObtenerTipado_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
